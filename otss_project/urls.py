75e9690c7249e460957dcdc7b2873da5f8b74319
Aniket Kolse <aniketkolse11@gmail.com>
2026-01-05 09:28:35 +0530

    UI enhancement: improved login, register, orders, invoice, admin dashboard

--- otss_project/urls.py
+++ otss_project/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('accounts/', include('accounts.urls')),
-
-    # USER SIDE
     path('', include('tiffin.urls')),
-
-    # ORDERS + DASHBOARD + REPORTS
     path('', include('orders.urls')),
-
-    # INVOICES
     path('', include('invoices.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tiffin/models.py
+++ tiffin/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Tiffin(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    image = models.ImageField(upload_to='tiffins/', blank=True, null=True)
+
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
